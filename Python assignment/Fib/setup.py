4639a199001eb5189af38ddffc9407df2d089bf2
PingChongHO <pcho1967@gmail.com>
2014-11-15 14:38:26 +0800

    C assignment
    C assignment

---
+++ Python assignment/Fib/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='Fib',
+      version='0.1',
+      description='Fib package',
+      url='https://github.com/gangchen/CUHK-I2P',
+      author='PC HO 1155057016',
+      author_email='pcho1967@gmail.com',
+      license='GPLv3',
+      packages=['Fib'],
+      zip_safe=False)
